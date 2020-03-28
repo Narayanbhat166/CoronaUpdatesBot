7f68c71295da4fbb9bcbec2070de4f0552f1192a
Narayanbhat166 <48803246+Narayanbhat166@users.noreply.github.com>
2020-03-28 16:57:48 +0530

    Add files via upload

--- worldmetres.py
+++ worldmetres.py
@@ -1,8 +1,22 @@
 import requests
 import time
+import datetime
 from bs4 import BeautifulSoup
 
 FILE_NAME = 'Infected_count.dat'
+
+
+def telegram_bot_sendtext(bot_message):
+
+    bot_token = '1132521081:AAHht4z1Eo00E86wcIZtGqYH1CshBxnQyk8'
+    bot_chatID = '784469586'
+    send_text = 'https://api.telegram.org/bot' + bot_token + \
+        '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+
+    response = requests.get(send_text)
+
+    return response.json()
+
 
 URL = 'https://www.worldometers.info/coronavirus/country/india/'
 
@@ -23,5 +37,13 @@ while True:
         print(str(present - last) + " New Infections")
         with open(FILE_NAME, 'w') as f:
             f.write(str(present))
+
+        test = telegram_bot_sendtext(
+            "New Infections in India: "+str(present - last))
+        if test['ok'] == True:
+            print("Sent a Telegram message")
+    else:
+        print("last checked at: " +
+              str(datetime.datetime.now().strftime('%d/%m/%Y %H:%M')))
 
     time.sleep(30)
