f309d14935a69ad14ad025f226f89f21bd54b805
Narayanbhat166 <48803246+Narayanbhat166@users.noreply.github.com>
2020-06-26 21:45:38 +0530

    Removed my bot token and chat-id

--- worldmetres.py
+++ worldmetres.py
@@ -8,8 +8,8 @@ FILE_NAME = 'Infected_count.dat'
 
 def telegram_bot_sendtext(bot_message):
 
-    bot_token = '1132521081:AAHht4z1Eo00E86wcIZtGqYH1CshBxnQyk8'
-    bot_chatID = '784469586'
+    bot_token = <bot token here>
+    bot_chatID = <chat-id>
     send_text = 'https://api.telegram.org/bot' + bot_token + \
         '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
 
